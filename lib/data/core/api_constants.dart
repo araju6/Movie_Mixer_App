ec50f6fed9c6e83ea67fdfba7636d5637caa8030
Aditya Raju <61385307+araju6@users.noreply.github.com>
2024-05-18 22:31:41 +0530

    Update api_constants.dart

--- lib/data/core/api_constants.dart
+++ lib/data/core/api_constants.dart
@@ -2,6 +2,6 @@ class ApiConstants{
   ApiConstants._();
 
   static const String BASE_URL = "https://api.themoviedb.org/3/";
-  static const String API_KEY = "c0f3c3514a9a4f6d4c5d0a45c8821d98";
+  static const String API_KEY = "";
   static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 }
